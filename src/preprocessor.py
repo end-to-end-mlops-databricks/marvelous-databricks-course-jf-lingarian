ba11e282b5624bb49466a07678b0eaf1d2acf7d9
jfigura96 <jakub.figur96@gmail.com>
2024-10-21 16:51:51 +0200

    pre-commit

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,6 +1,9 @@
-import polars as pl
 import datetime as dt
 import os
+from typing import List, Optional
+
+import polars as pl
+
 
 class DataProcessor:
     """
@@ -21,7 +24,7 @@ class DataProcessor:
         """
         self.file_path = file_path
         self.file_name = file_name
-        self.preprocessed_data_file_path = f'data/preprocessed/{file_name}.parquet'
+        self.preprocessed_data_file_path = f"data/preprocessed/{file_name}.parquet"
 
     def process_files(self):
         """
@@ -35,7 +38,7 @@ class DataProcessor:
 
         # Filter and process the names of .csv files
         for file in files:
-            if file.startswith(self.file_name) and file.endswith('.csv'):
+            if file.startswith(self.file_name) and file.endswith(".csv"):
                 print(file)
                 self.merge_new_data_with_preprocessed_data(file)
         return pl.read_parquet(self.preprocessed_data_file_path)
@@ -51,25 +54,37 @@ class DataProcessor:
             pl.DataFrame: The transformed DataFrame with categorical columns and a unique identifier.
         """
         return (
-            df
-            .with_columns(
+            df.with_columns(
                 [
                     pl.col("Client").cast(pl.Utf8).cast(pl.String),
                     pl.col("Warehouse").cast(pl.Utf8).cast(pl.String),
                     pl.col("Product").cast(pl.Utf8).cast(pl.String),
-                    pl.concat_str([pl.col('Client'), pl.lit('/'), pl.col('Warehouse'), pl.lit('/'), pl.col('Product')]).cast(pl.String).alias('unique_id')
+                    pl.concat_str(
+                        [
+                            pl.col("Client"),
+                            pl.lit("/"),
+                            pl.col("Warehouse"),
+                            pl.lit("/"),
+                            pl.col("Product"),
+                        ]
+                    )
+                    .cast(pl.String)
+                    .alias("unique_id"),
                 ]
             )
-            .unpivot(index=['unique_id','Product','Warehouse','Client'], variable_name='ds', value_name='y')
-            [['ds','y','unique_id','Client','Warehouse','Product']]
-            .with_columns(pl.col('ds').cast(pl.Date))
-            .sort(by=['unique_id', 'ds'])
+            .unpivot(
+                index=["unique_id", "Product", "Warehouse", "Client"],
+                variable_name="ds",
+                value_name="y",
+            )[["ds", "y", "unique_id", "Client", "Warehouse", "Product"]]
+            .with_columns(pl.col("ds").cast(pl.Date))
+            .sort(by=["unique_id", "ds"])
         )
 
     def merge_new_data_with_preprocessed_data(
         self,
         new_data_file_name: str,
-        original_cat_cols: list = ['Client','Warehouse','Product']      
+        original_cat_cols: Optional[List[str]] = None,
     ) -> None:
         """
         Merge new data with preprocessed data and save the result.
@@ -82,8 +97,17 @@ class DataProcessor:
         Returns:
             None
         """
+        if original_cat_cols is None:
+            original_cat_cols = ["Client", "Warehouse", "Product"]
+
         # Read new file
-        new_df = pl.read_csv(f'data/raw/{new_data_file_name}').with_columns([pl.col('Client').cast(pl.String).cast(pl.Categorical), pl.col('Warehouse').cast(pl.String).cast(pl.Categorical), pl.col('Product').cast(pl.String).cast(pl.Categorical)])
+        new_df = pl.read_csv(f"data/raw/{new_data_file_name}").with_columns(
+            [
+                pl.col("Client").cast(pl.String).cast(pl.Categorical),
+                pl.col("Warehouse").cast(pl.String).cast(pl.Categorical),
+                pl.col("Product").cast(pl.String).cast(pl.Categorical),
+            ]
+        )
 
         # Read the preprocessed data
         old_df = pl.read_parquet(self.preprocessed_data_file_path)
@@ -92,31 +116,34 @@ class DataProcessor:
         dates_from_new_file = [col for col in new_df.columns if col not in original_cat_cols]
 
         # Convert the date columns to datetime format
-        date_from_new_file_datetime_format = [dt.datetime.strptime(date, '%Y-%m-%d').date() for date in dates_from_new_file]
+        date_from_new_file_datetime_format = [
+            dt.datetime.strptime(date, "%Y-%m-%d").date() for date in dates_from_new_file
+        ]
 
         # Get the unique dates from the old file
-        dates_from_old_file = old_df['ds'].unique().to_list()
+        dates_from_old_file = old_df["ds"].unique().to_list()
 
         # Get the dates that are not overlapping
-        dates_that_are_not_overlapping = [col for col in date_from_new_file_datetime_format if col not in dates_from_old_file]
+        dates_that_are_not_overlapping = [
+            col for col in date_from_new_file_datetime_format if col not in dates_from_old_file
+        ]
 
         if not dates_that_are_not_overlapping:
-            print('All dates are overlapping')
+            print("All dates are overlapping")
         else:
             # Process the new data
             new_df_processed = (
                 self.prepare_raw_csv(new_df)
-                .with_columns(pl.col('y').cast(pl.Float64))
-                .filter(pl.col('ds').is_in(dates_that_are_not_overlapping))
+                .with_columns(pl.col("y").cast(pl.Float64))
+                .filter(pl.col("ds").is_in(dates_that_are_not_overlapping))
             )
 
             with pl.StringCache():
                 # Append the new data to the old data, sort and write to parquet
                 (
-                    pl
-                    .concat([old_df, new_df_processed], how='vertical')
-                    .sort(by=['unique_id', 'ds'])
+                    pl.concat([old_df, new_df_processed], how="vertical")
+                    .sort(by=["unique_id", "ds"])
                     .write_parquet(self.preprocessed_data_file_path)
                 )
-                
+
             print(f"Data for file {new_data_file_name} has been successfully merged with the preprocessed data")
